09e4bb6fd45b070c5493bed28f15db7f31b4e114
Carlos Villarta <villarta.carlos.b.7725@gmail.com>
2023-09-10 09:49:54 +0800

    fix: layout for larger screen

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,7 +18,10 @@ export default function HomePage() {
                     <h1 className='location'>SPACE</h1>
                     <p className='desc'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
-                <Link to='destination' className='explore-btn'>EXPLORE</Link>
+                <div className='explore-container'>
+                    <Link to='destination' className='explore-btn'>EXPLORE</Link>
+                </div>
+                
             </main>
         </>
     )
